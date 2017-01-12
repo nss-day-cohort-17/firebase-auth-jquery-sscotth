00bff7fb18ee4390432ab6357c9695ac392163fc
Scott Humphries <git@sscotth.io>
2017-01-12 11:54:48 -0600

    Add firebase login

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+firebase.initializeApp({
+  apiKey: 'AIzaSyDfKMbHscf_4oQ5oL0EGh0u6zP9Gunp9l0',
+  authDomain: 'c17-jquery-auth.firebaseapp.com',
+  databaseURL: 'https://c17-jquery-auth.firebaseio.com',
+  storageBucket: 'c17-jquery-auth.appspot.com',
+  messagingSenderId: '936314115006',
+})
+
+firebase.auth().onAuthStateChanged(() => {
+  if (firebase.auth().currentUser) {
+    // logged in
+    var email = firebase.auth().currentUser.email
+
+    $('.login-page').addClass('hidden')
+    $('.main-page').removeClass('hidden')
+    $('.main-page h1').text(`Welcome ${email}`)
+  } else {
+    // logged out
+    $('.login-page').removeClass('hidden')
+    $('.main-page').addClass('hidden')
+  }
+})
+
 $('form').submit((e) => {
   var email = $('input[type="email"]').val()
-  $('.main-page h1').text(`Welcome ${email}`)
-  $('.login-page').addClass('hidden')
-  $('.main-page').removeClass('hidden')
+  var password = $('input[type="password"]').val()
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => $('form')[0].reset())
+
   e.preventDefault()
 })
